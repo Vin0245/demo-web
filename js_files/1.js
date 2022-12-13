3e1357a9058ecd052bcc0546ee00ee2b7ad218f2
kunaljoshi <kunaljoshi805@gmail.com>
2022-12-13 11:41:05 +0530

    my second commit

---
+++ js_files/1.js
@@ -0,0 +1,8 @@
+var fullname="kunal joshi";
+var password="123456";
+var courseName="JS Bootcamp";
+var isLoggedIn=false;
+var loggedCount=43;
+console.log(loggedCount);
+var paymentMode;
+console.log(paymentMode);
