56e55092f9693d601e5cca41c697cb75dd8b6122
kunaljoshi <kunaljoshi805@gmail.com>
2023-02-14 14:27:43 +0530

    my 25th commit

---
+++ JavaScript_Practice/pra54.js
@@ -0,0 +1,4 @@
+// pop method in Array
+let maths=[1,4,9,16,25];
+console.log(maths.pop());
+console.log(maths);
---
+++ JavaScript_Practice/pra56.js
@@ -0,0 +1,5 @@
+// unshfit method in array
+
+let fruits=['Apple','Bada Apple','Chota Apple','Double Apple'];
+fruits.unshift('Apple1','Apple2','apple3');
+console.log(fruits);
