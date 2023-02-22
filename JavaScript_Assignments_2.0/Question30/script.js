5fcd027ce75797d79ccbfebd32cfad322630a705
kunaljoshi <kunaljoshi805@gmail.com>
2023-02-22 17:26:08 +0530

    my 32th commit

---
+++ JavaScript_Assignments_2.0/Question30/script.js
@@ -0,0 +1,19 @@
+function calculator(a,b){
+    function add(){
+        return a+b;
+    }
+    console.log(add())
+    function sub(){
+        return a-b;
+    }
+    console.log(sub())
+    function max(){
+        return a*b;
+    }
+    console.log(max())
+    function div(){
+        return a/b;
+    }
+    console.log(div())
+}
+calculator(4,6)
